665b25bfe83373928bb36a323f1f53ecf1f5c749
svenschilling <sven.schilling@gmail.com>
2021-10-10 19:53:28 +0200

    first upload

---
+++ squareNSum.py
@@ -0,0 +1,15 @@
+"""
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+"""
+numbers = 1,2
+
+def square_sum(numbers):
+    y = 0
+    for x in numbers:
+        y += pow(x,2)
+    return y
+        
+square_sum(numbers)
+
