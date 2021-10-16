f3bedad959d7dcc80fc2400a0917c6d30c8790eb
svenschilling <sven.schilling@gmail.com>
2021-10-16 23:51:08 +0200

    update

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-string = "gergh"
-new_string = string[:-1]
+import string
+
+bla = "gergh    terter    tert    zezer               gdfcf"
+
+new_string = bla[:-1]
 print(new_string)
+print(string.capwords(bla))
+
+strng ="fsdrmne32"
+for x in strng:
+    if x.isdigit():
+        print(x + " digit found")
+        if int(x) == 3:
+            print("drei gefunden")
+    else:
+        print(x + " is a char")
+    pass
+    
