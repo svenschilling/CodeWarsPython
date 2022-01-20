9dfaf92780faaf402ce5f7b22287fa1a0e3996d4
svenschilling <sven.schilling@gmail.com>
2022-01-20 21:13:31 +0100

    +

---
+++ SumOfFirstNthTermSeries/main.py
@@ -0,0 +1,25 @@
+'''
+    Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+    Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+    Rules:
+    You need to round the answer to 2 decimal places and return it as String.
+
+    If the given value is 0 then it should return 0.00
+
+    You will only be given Natural Numbers as arguments.
+
+    Examples:(Input --> Output)
+    1 --> 1 --> "1.00"
+    2 --> 1 + 1/4 --> "1.25"
+    5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.
+'''
+
+from fractions import Fraction
+
+bruch1 = Fraction(1,4)
+bruch2 = Fraction(1,7)
+bruch3 = Fraction(1,10)
+bruch4 = Fraction(1,13)
+
+print(float(1 + bruch1 + bruch2 + bruch3 + bruch4))
